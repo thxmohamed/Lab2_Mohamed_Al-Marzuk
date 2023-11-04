623f86cf819a4b73d0cd4db4ac83e24888085a3a
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2023-11-03 22:52:50 -0300

    Se agrega el RF5

--- TDAChatbot_22594262_Al-Marzuk.pl
+++ TDAChatbot_22594262_Al-Marzuk.pl
@@ -1,1 +1,6 @@
 %Aquí van las funciones del TDA Chatbot
+
+chatbot(ChatbotID, ChatbotName, WelcomeMsg, StartFlowID, Flows,
+        [ChatbotID, ChatbotName, WelcomeMsg, StartFlowID, FlowsSinDuplicados]) :-
+    agregarSinDuplicados(Flows, [], FlowsSinDuplicados),
+    integer(ChatbotID), string(ChatbotName), string(WelcomeMsg), integer(StartFlowID), is_list(Flows).
